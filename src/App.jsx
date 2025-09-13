206d5c6b84ed53d7ac03571962e075908a30f2d8
HanXD345 <manpreet345@yahoo.com>
2025-09-13 19:21:08 -0400

    Initialize react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios';
+
+const App = () => {
+  const getObj = async () => {
+    await axios.get("http://localhost:8080/").then(res => console.log(res.data));
+  }
+
+  getObj();
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
